18b9365443ff0401f8e076f95d04b936f1062851
Fonseverin <diomon@pop-os.localdomain>
2019-10-20 14:13:35 +0300

    Futher development of BlackboardObjects

--- include/BlackboardObjects.h
+++ include/BlackboardObjects.h
@@ -1,23 +1,22 @@
-/**
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- */
-#ifndef BLACKBOARD_OBJECT_HPP
-#define BLACKBOARD_OBJECT_HPP
+#ifndef BLACKBOARD_OBJECTS_HPP
+#define BLACKBOARD_OBJECTS_HPP
 
+#include <list>
+#include <string>
 #include <vector>
+
+#include "KnowledgeSources.h"
 
 class Blackboard
 {
 private:
     std::vector<BlackboardObject*> objects;
 public:
-    Blackboard();
+    void Reset();
+    void AssertProblem();
+    void Connect();
+    int isSolved() const;
+    std::string retrieveSolution() const;
 };
 
 class BlackboardObject
@@ -25,10 +24,63 @@ class BlackboardObject
 private:
     Blackboard* blackboard;
 public:
-    BlackboardObject(Blackboard*, ...);
+    BlackboardObject(Blackboard*);
 
-    void* Register(...);
-    void* Resign(...);
+    void Register();
+    void Resign();
+    void Add();
 };
 
-#endif // BLACKBOARD_OBJECT_HPP
+class Alphabet 
+: public BlackboardObject, virtual public Dependent
+{
+public:
+    char plaintext(char) const;
+    char ciphertext(char) const;
+    int isBound(char) const;
+};
+
+class CipherLetter 
+: public BlackboardObject, virtual public Dependent
+{
+protected:
+    char letter;
+    Affirmation affirmations;
+public:
+    char value() const;
+    int isSolved() const; 
+};
+
+class Word 
+: public BlackboardObject, virtual public Dependent
+{
+protected:
+    std::list<CipherLetter*> letters;
+public:
+    Sentence& sentence() const;
+    Word* previous() const;
+    Word* next() const;
+};
+
+class Sentence
+: public BlackboardObject, virtual public Dependent
+{
+protected:
+    std::list<Word*> words;
+};
+
+
+class Assumption 
+: public BlackboardObject, virtual public Dependent
+{
+public:
+    BlackboardObject* target;
+    KnowledgeSource* creator;
+    std::string reason;
+    char plainLetter;
+    char cipherLetter;
+};
+
+class Affirmation {};
+
+#endif // BLACKBOARD_OBJECTS_HPP
---
+++ include/KnowledgeSources.h
@@ -0,0 +1,8 @@
+#ifndef KNOWLEDGE_SOURCES_H
+#define KNOWLEDGE_SOURCES_H
+
+class KnowledgeSource {};
+
+class Dependent {};
+
+#endif // KNOWLEDGE_SOURCES_H
